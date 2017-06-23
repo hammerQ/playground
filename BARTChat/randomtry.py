ea6358ce1d697630b1646ef88b81144868297fc0
Hammer <mu.qiao@gmail.com>
2017-06-23 16:13:09 -0700

    a working example of xml parsing to json

---
+++ BARTChat/randomtry.py
@@ -0,0 +1,10 @@
+# This Python file uses the following encoding: utf-8
+# encoding: utf-8
+__author__ = 'mqiao'
+
+import untangle
+
+#  testxml = '<xml><ToUserName><![CDATA[gh_bf693268132e]]></ToUserName><FromUserName><![CDATA[oMwf3s4hyHHp1eKxyyiCpb8is9oQ]]></FromUserName><CreateTime>1437238456</CreateTime><MsgType><![CDATA[text]]></MsgType><Content><![CDATA[Abc]]></Content><MsgId>6172892165281362744</MsgId></xml>'
+xml = '<?xml version="1.0"?><root><child name="child1"/></root>'
+obj = untangle.parse(xml)
+print("JSON is " + obj.root.child['name']) # 'child1'
