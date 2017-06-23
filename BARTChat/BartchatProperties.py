45d596267c9e9bf54ee3d0eea280b3740b3d5dce
Hammer <mu.qiao@gmail.com>
2017-06-23 15:55:04 -0700

    add a property file

---
+++ BARTChat/BartchatProperties.py
@@ -0,0 +1,11 @@
+# This Python file uses the following encoding: utf-8
+# encoding: utf-8
+__author__ = 'mqiao'
+
+
+class BARTAPI:
+    """Contains properties that sit on BART side
+    """
+    BART_API_ENDPOINT = "http://api.bart.gov/api/etd.aspx?"
+    BART_API_KEY = "MW9S-E7SL-26DU-VV8V"
+    BART_EXAMPLE_REQUEST = "http://api.bart.gov/api/etd.aspx?cmd=etd&orig=RICH&key=MW9S-E7SL-26DU-VV8V"
